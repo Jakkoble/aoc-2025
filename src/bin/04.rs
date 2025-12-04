376cad8eca198a6590bf6ad850600c4e943996c0
Jakob Wassertheurer <jwassertheurer.mmt-b2024@fh-salzburg.ac.at>
2025-12-04 21:44:39 +0100

    refactor code

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,43 +1,15 @@
 advent_of_code::solution!(4);
 
-fn parse_input(input: &str) -> Vec<Vec<char>> {
-    input
-        .lines()
-        .map(|line| line.trim().chars().collect())
-        .collect()
-}
-
-fn count_neighbours(rows: &Vec<Vec<char>>, row: i32, col: i32) -> u32 {
-    let mut count = 0;
-    for r in 0.max(row - 1)..(row + 2).min(rows.len().try_into().unwrap()) {
-        for c in 0.max(col - 1)..(col + 2).min(rows[0].len().try_into().unwrap()) {
-            if r == row && c == col {
-                continue;
-            }
-            if rows[r as usize][c as usize] == '@' {
-                count += 1;
-            }
-        }
-    }
-    count
-}
-
 pub fn part_one(input: &str) -> Option<u64> {
-    let rows = parse_input(input);
+    let grid = Grid::new(input);
     let mut count = 0;
 
-    for r in 0..rows.len() {
-        let row = &rows[r];
-
-        for c in 0..row.len() {
-            let cell = row[c];
-
-            if cell != '@' {
-                continue;
-            }
-
-            if count_neighbours(&rows, r.try_into().unwrap(), c.try_into().unwrap()) < 4 {
-                count += 1;
+    for x in 0..grid.width {
+        for y in 0..grid.height {
+            if let Some('@') = grid.get(x, y) {
+                if grid.count_neighbours(x, y) < 4 {
+                    count += 1;
+                }
             }
         }
     }
@@ -46,22 +18,20 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut rows = parse_input(input);
+    let mut grid = Grid::new(input);
     let mut count = 0;
 
     loop {
         let mut removed_rolls = 0;
 
-        for r in 0..rows.len() {
-            for c in 0..rows[r].len() {
-                if rows[r][c] != '@' {
-                    continue;
-                }
-
-                if count_neighbours(&rows, r.try_into().unwrap(), c.try_into().unwrap()) < 4 {
-                    count += 1;
-                    removed_rolls += 1;
-                    rows[r][c] = '.';
+        for x in 0..grid.width {
+            for y in 0..grid.height {
+                if let Some('@') = grid.get(x, y) {
+                    if grid.count_neighbours(x, y) < 4 {
+                        count += 1;
+                        removed_rolls += 1;
+                        grid.set(x, y, '.');
+                    }
                 }
             }
         }
@@ -72,6 +42,66 @@ pub fn part_two(input: &str) -> Option<u64> {
     }
 
     Some(count)
+}
+
+#[derive(Debug)]
+struct Grid {
+    height: usize,
+    width: usize,
+    data: Vec<char>,
+}
+
+impl Grid {
+    fn new(input: &str) -> Self {
+        let lines: Vec<&str> = input.lines().collect();
+        if lines.is_empty() {
+            return Self {
+                height: 0,
+                width: 0,
+                data: vec![],
+            };
+        }
+
+        Self {
+            height: lines.len(),
+            width: lines[0].len(),
+            data: lines.iter().flat_map(|line| line.chars()).collect(),
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> Option<char> {
+        if x >= self.width || y >= self.width {
+            None
+        } else {
+            Some(self.data[self.width * y + x])
+        }
+    }
+
+    fn set(&mut self, x: usize, y: usize, value: char) {
+        if x >= self.width || y >= self.height {
+            return;
+        }
+
+        self.data[self.width * y + x] = value;
+    }
+
+    fn count_neighbours(&self, x: usize, y: usize) -> usize {
+        let mut count = 0;
+
+        for r in y.saturating_sub(1)..(y + 2).min(self.height) {
+            for c in x.saturating_sub(1)..(x + 2).min(self.width) {
+                if r == y && c == x {
+                    continue;
+                }
+
+                if let Some('@') = self.get(c, r) {
+                    count += 1;
+                }
+            }
+        }
+
+        count
+    }
 }
 
 #[cfg(test)]
